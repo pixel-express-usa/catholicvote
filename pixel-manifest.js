00166c42163b85e719ac8f2b7ec6919b99b13cb6
pixel-express-usa <randy@americastoplists.com>
2025-11-03 09:11:03 -0700

    Update pixel-manifest.js

--- pixel-manifest.js
+++ pixel-manifest.js
@@ -1,4 +1,4 @@
 // Commit-pinned URL to your working script:
-window.PXL_URL = "https://cdn.jsdelivr.net/gh/pixel-express-usa/catholicvote@d4f876929efb0ed982abd649f29f757a14ecec12/pixel-script.js";
+window.PXL_URL = "https://cdn.jsdelivr.net/gh/pixel-express-usa/catholicvote@905a53f3ca4bde42b53ed56c7a9229c82ecb7710/pixel-script.js";
 // Optional tracer for sanity:
 console.log("PXL_URL ->", window.PXL_URL);
